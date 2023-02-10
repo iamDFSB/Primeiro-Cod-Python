f9fef1325d4c5509173669f1daeae936c1ba8389
Daniel Felipe Santana Batista <daniel.sbatista@shopeemobile-external.com>
2023-02-10 08:02:53 -0300

    inicializamos o projeto

---
+++ primeirocod.py
@@ -0,0 +1,6 @@
+# Pintar a Parede 
+altura=float(input('Qual a altura da parede em metros? '))
+largura=float(input('Quala largura da parede em metros? '))
+area=altura*largura
+result=area/2
+print('A quantidade de tinta necessária é: {:.0f} litros'.format(result))
