792cfca5438866f1628768eef9b0f0e58b34995c
Daniel Felipe Santana Batista <daniel.sbatista@shopeemobile-external.com>
2023-02-10 09:12:47 -0300

    Defini a largura da parede2

--- primeirocod.py
+++ primeirocod.py
@@ -1,6 +1,7 @@
 # Pintar a Parede 
 # Defini a altura da parede 
 altura=float(input('Qual a altura da parede em metros? '))
+# Defini a largura da parede
 largura=float(input('Quala largura da parede em metros? '))
 area=altura*largura
 result=area/2
