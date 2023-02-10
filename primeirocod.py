1c306f01c3455f76bdc273775d15e9874eb6ea99
Daniel Felipe Santana Batista <daniel.sbatista@shopeemobile-external.com>
2023-02-10 09:38:57 -0300

    Coloquei o Jogo da tabuada

--- primeirocod.py
+++ primeirocod.py
@@ -3,11 +3,15 @@
 altura=float(input('Qual a altura da parede em metros? '))
 # Defini a largura da parede
 largura=float(input('Quala largura da parede em metros? '))
+# Descobri a área da parede
 area=altura*largura
 result=area/2
 print('A quantidade de tinta necessária é: {:.0f} litros'.format(result))
 # Outro exercicio a baixo
 # Tabuada
+print('Começou o Jogo da tabuada\n')
+print('-'*20)
 num=int(input('Digite um número: '))
 for i in range(11):
     print('Seu resultado é: {} X {} = {}'.format(num,i,num*i))
+print('-'*20)
